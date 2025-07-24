d639b328a9ab6b5ad6fd3881d9d59350afa4f790
Divyanshu Raj <divyanshuraj@Divyanshus-MacBook-Pro.local>
2025-07-24 10:11:04 +0530

    adding setting page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import Settings from "./pages/Settings";
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <div className="container main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App;
